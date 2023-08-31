eb9809d18c6d53e7eec8520e0a4c522009f5dfec
Evgenii Fedotov <fedotoves@gmail.com>
2023-08-30 20:17:19 -0700

    fix

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 locals {
-  ingress_rules = tomap(concat([
+  ingress_rules = concat([
+    {
+      protocol        = 6
+      from_port       = 0
+      to_port         = 20200
+      cidr_blocks     =
+      security_groups = null
+      self            = true
+    },
+    ], [
     for r in var.ingress_rules : {
       protocol        = lookup(r, "protocol", 6)
       from_port       = lookup(r, "port", 0)
@@ -8,9 +17,17 @@ locals {
       security_groups = lookup(r, "security_groups", null)
       self            = false
     }
-  ]))
+  ])
 
-  egress_rules = tomap(concat([
+  egress_rules = concat([
+    {
+      protocol        = 6
+      from_port       = 0
+      to_port         = 20200
+      cidr_blocks     = "0.0.0.0/0"
+      security_groups = null
+    }
+    ], [
     for r in var.egress_rules : {
       protocol        = lookup(r, "protocol", 6)
       from_port       = lookup(r, "port", 0)
@@ -18,7 +35,7 @@ locals {
       cidr_blocks     = lookup(r, "cidr_blocks", "0.0.0.0/0")
       security_groups = lookup(r, "security_groups", null)
     }
-  ]))
+  ])
 }
 
 resource "oci_core_network_security_group" "ocisecuritygroup" {
